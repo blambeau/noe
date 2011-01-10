62f7fd9d706bea1ddaca443c0c07c1162b4a6a8a
Bernard Lambeau <blambeau@gmail.com>
2011-01-10 11:23:09 +0100

    Refactored create command to use new Template and Template::Entry tools.

--- lib/noe/create.rb
+++ lib/noe/create.rb
@@ -26,56 +26,44 @@ module Noe
           self.dry_run = true
         }
       end
-
-      def newname(file, user_info)
-        newname = File.basename(file)
-        if newname =~ /__([a-z]+)__/
-          varname = $1
-          if x = user_info['variables'][varname]
-            newname.gsub(/__([a-z]+)__/, x)
-          else
-            raise "Missing variable #{varname}"
-          end
-        else
-          newname
-        end
-      end
-
-      # Instantiates a file
-      def instantiate(file, user_info, where)
-        return if ['.', '..'].include?(File.basename(file))
-        newname = newname(file, user_info)
-        newfile  = File.join(where, newname)
-        if File.directory?(file)
-          if dry_run
-            puts "mkdir #{newfile}"
-          else
-            FileUtils.mkdir(newfile)
-          end
-          Dir.foreach(file) do |child|
-            instantiate(File.join(file, child), user_info, newfile)
-          end
-        elsif File.file?(file)
-          if dry_run
-            puts "#{file} -> #{newfile}"
-          else
-            File.open(newfile, 'w') do |out|
-              context = user_info['variables']
-              dialect = "wlang/active-string"
-              out << WLang::file_instantiate(file, context, dialect)
-            end
-          end
+      
+      def do_dry!(entry, variables)
+        relocated = entry.relocate(variables)
+        if entry.file?
+          puts "#{entry.path} -> #{relocated}"
+        elsif entry.directory?
+          puts "mkdir #{relocated}"
         end
       end
       
-      # Run the command
+      def do_real!(entry, variables)
+        relocated = entry.relocate(variables)
+        if entry.file?
+          File.open(relocated, 'w') do |out|
+            dialect = "wlang/active-string"
+            out << WLang::file_instantiate(entry.realpath, variables, dialect)
+          end
+        elsif entry.directory?
+          FileUtils.mkdir(relocated)
+        end
+      end
+      
       def execute(args)
         raise Quickl::Help unless args.size == 2
         template_name, info_file = args
+
+        # load template and user infos
         template = template(template_name)
         user_info = YAML::load(File.read(info_file))
-        Dir.foreach(template.src_folder) do |root|
-          instantiate(File.join(template.src_folder, root), user_info, '.')
+        variables = user_info['variables']
+        
+        # instantiate the template now
+        template.visit do |entry|
+          if dry_run
+            do_dry!(entry, variables)
+          else
+            do_real!(entry, variables)
+          end
         end
       end
 
