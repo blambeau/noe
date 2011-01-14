86809af3c5719bb015d9c8ff51a49fc932d876fe
Bernard Lambeau <blambeau@gmail.com>
2011-01-14 09:13:49 +0100

    Fixed a bug in generated Loader.require

--- templates/ruby/src/lib/__lower__/loader.rb
+++ templates/ruby/src/lib/__lower__/loader.rb
@@ -2,15 +2,15 @@ module !{upper}
   module Loader
     
     def require(name, version = nil)
-      require name
+      Kernel.require name
     rescue LoadError
       retried = true
       require "rubygems"
       gem name, version || ">= 0"
-      require name
+      Kernel.require name
     end
     module_function :require
     
   end # module Loader
 end # module !{upper}
-*{dependencies.select{|dep| dep.groups.include?('runtime')} as dep}{!{upper}::Loader.require(+{dep.name}, +{dep.version})}{!"\n"}
+*{dependencies.select{|dep| dep.groups.include?('runtime')} as dep}{!{upper}::Loader.require(+{dep.name}, +{dep.version})}{!{"\n"}}
