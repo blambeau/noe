f6fccac16fc5e15273cf076a3d7d2aa8f8e43d46
Bernard Lambeau <blambeau@gmail.com>
2011-01-14 09:34:56 +0100

    Documented the generated loader.

--- templates/ruby/src/lib/__lower__/loader.rb
+++ templates/ruby/src/lib/__lower__/loader.rb
@@ -1,13 +1,41 @@
 module !{upper}
+  #
+  # This module provides tools to load stdlib and gem dependencies.
+  #
   module Loader
     
+    #
+    # This method allows requiring dependencies with some flexibility.
+    #
+    # If ALWAYS attempt a simple <code>Kernel.require(name)</code> before 
+    # anything else. If this require fails with a LoadError then it falls
+    # back requiring the gem with specified version (defaults to >= 0) then
+    # retrying the require. Making so allows you to twist the load path 
+    # during development, therefore bypassing version requirement to test 
+    # your lib under new versions of some gems.
+    #
+    # Examples:
+    #
+    #   # Require something from the standard library
+    #   !{upper}::Loader.require('fileutils')
+    #
+    #   # Require a gem without specifing any particular version
+    #   !{upper}::Loader.require('highline')
+    #
+    #   # Require a gem, specifing a particular version
+    #   !{upper}::Loader.require('highline', "~> 1.6")
+    #
+    #   # Twist the load path to use version of highline you've recently 
+    #   # forked (this bypass the version requirement)
+    #   $LOAD_PATH.unshift ...   # or ruby -I...
+    #   !{upper}::Loader.require('highline', "~> 1.6")
+    #
     def require(name, version = nil)
-      Kernel.require name
+      Kernel.require name.to_s
     rescue LoadError
-      retried = true
       require "rubygems"
-      gem name, version || ">= 0"
-      Kernel.require name
+      gem name.to_s, version || ">= 0"
+      Kernel.require name.to_s
     end
     module_function :require
     
