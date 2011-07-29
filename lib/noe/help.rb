17fe8fbbb34bff91c960ee2e77cff83faf012064
Bernard Lambeau <blambeau@gmail.com>
2011-07-29 10:46:31 +0200

    Updated help to match quickl 0.3.0

--- lib/noe/help.rb
+++ lib/noe/help.rb
@@ -14,10 +14,10 @@ module Noe
       # Command execution
       def execute(args)
         if args.size != 1
-          puts self.class.super_command.help
+          puts Quickl.help(Quickl.super_command(self))
         else
-          cmd = Quickl.has_subcommand!(self.class.super_command, args.first)
-          puts cmd.help
+          cmd = Quickl.subcommand!(Quickl.super_command(self), args.first)
+          puts Quickl.help(cmd)
         end
       end
       
