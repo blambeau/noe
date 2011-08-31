e69c93f98a5045189c08b9d26d60bdcf87f4ec42
eregon <eregontp@gmail.com>
2011-08-31 16:44:03 +0200

    make the line endings and trailing spaces test pass
    * note it pass only if there is at least one exclusive runtime dependency and another, and links is not empty

--- templates/ruby/src/lib/__lower__.rb
+++ templates/ruby/src/lib/__lower__.rb
@@ -2,7 +2,7 @@
 # !{summary}
 #
 module !{upper}
-  
+
 end # module !{upper}
 require "!{lower}/version"
 require "!{lower}/loader"
--- templates/ruby/src/lib/__lower__/version.rb
+++ templates/ruby/src/lib/__lower__/version.rb
@@ -1,14 +1,14 @@
 module !{upper}
   module Version
-  
+
     MAJOR = +{version.split('.')[0].to_i}
     MINOR = +{version.split('.')[1].to_i}
     TINY  = +{version.split('.')[2].to_i}
-  
+
     def self.to_s
       [ MAJOR, MINOR, TINY ].join('.')
     end
-  
-  end 
+
+  end
   VERSION = Version.to_s
 end
